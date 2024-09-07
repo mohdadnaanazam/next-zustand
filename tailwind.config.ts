fba0431330d9ae895bd2d56e2a96312a803ead5e
mohdadnaanazam <adnaan28.azam@gmail.com>
2024-09-07 21:55:16 +0530

    intial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+  	container: {
+  		center: 'true',
+  		padding: {
+  			DEFAULT: '1rem',
+  			md: '1.5rem',
+  			lg: '2rem'
+  		}
+  	},
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")]
+}
